b0c4c20e0029ac5b1bfd6f422b209ec74877436f
CaptainBoggle <captainboggle108@gmail.com>
2019-08-01 21:11:50 +1000

    Count card function done

--- bigtwo.py
+++ bigtwo.py
@@ -10,10 +10,11 @@ def ishigher(card1, card2):
 
     return True
 
-def countcards(ourDeck: list=None, opponentPlays: list=None):
+def countcards(hand: list=None, roundHistory: list=None):
     """Attempts to guess remaining cards. Very WIP."""
-    maybeRemain = list(set(opponentPlays).difference(allcards))
-    maybeRemain = list(set(maybeRemain).difference(ourDeck))
+    history = str(roundHistory)
+    opponentcards = [cards for cards in allcards if history.count(cards)]
+    return set(allcards).difference(hand, opponentcards)
 
 def findlegal(hand: list=None, playToBeat: list=None, isStartOfRound: bool=None, playType: int=None):
     """Finds legal moves with your hand and the current play to beat."""
@@ -24,3 +25,5 @@ def findlegal(hand: list=None, playToBeat: list=None, isStartOfRound: bool=None,
     if playType == 5: pass # 5-card plays
     
     return sortcards(legal)
+
+print(countcards([], [[[1, ['3D']], [2, ['4C']], [3, []], [0, ['8C']], [1, ['9S']], [2, ['0H']], [3, []], [0, ['0S']], [1, ['JD']], [2, ['JH']], [3, []], [0, ['KC']], [1, []], [2, ['2H']], [3, []], [0, ['2S']], [1, []], [2, []], [3, []]], []]))
