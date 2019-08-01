bdaf8ecc854d3062b113fa9f2e1def98766e0dc0
larrabyte <larrabyte@protonmail.ch>
2019-08-01 21:21:42 +1000

    Small updates
    docstrings changed and stuff

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # magnus-carlsen
 Python bot for playing Chinese poker (Big Two).
+Made to compete in the NCSS Advanced challenge for 2019!
--- bigtwo.py
+++ bigtwo.py
@@ -4,26 +4,23 @@ def cardvalue(card): return dict[card[0]] + dict[card[-1]]
 def sortcards(cardlist): return sorted(cardlist, key=cardvalue)
 
 def ishigher(card1, card2):
+    """Checks whether `card1` is higher than `card2`."""
     if dict[card1[0]] < dict[card2[0]]: return False
     elif dict[card1[0]] == dict[card2[0]]:
         if dict[card1[1]] <= dict[card2[1]]: return False
 
     return True
 
-def countcards(hand: list=None, roundHistory: list=None):
-    """Attempts to guess remaining cards. Very WIP."""
-    history = str(roundHistory)
-    opponentcards = [cards for cards in allcards if history.count(cards)]
+def countcards(hand, roundHistory: list=None):
+    """Returns cards that have not been played."""
+    opponentcards = [cards for cards in allcards if str(roundHistory).count(cards)]
     return set(allcards).difference(hand, opponentcards)
 
 def findlegal(hand: list=None, playToBeat: list=None, isStartOfRound: bool=None, playType: int=None):
     """Finds legal moves with your hand and the current play to beat."""
-
     if playType == 1: legal = [immigrant for immigrant in hand if ishigher(immigrant, playToBeat[0])]
     if playType == 2: pass # 2-card plays
     if playType == 3: pass # 3-card plays
     if playType == 5: pass # 5-card plays
     
     return sortcards(legal)
-
-print(countcards([], [[[1, ['3D']], [2, ['4C']], [3, []], [0, ['8C']], [1, ['9S']], [2, ['0H']], [3, []], [0, ['0S']], [1, ['JD']], [2, ['JH']], [3, []], [0, ['KC']], [1, []], [2, ['2H']], [3, []], [0, ['2S']], [1, []], [2, []], [3, []]], []]))
