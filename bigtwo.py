7416ed2f7c4d8fb175fe6bd46b2556df95361b5f
larrabyte <larrabyte@protonmail.ch>
2019-08-01 17:18:43 +1000

    Start on card counting
    ryzen > shintel
    change my thread

--- bigtwo.py
+++ bigtwo.py
@@ -1,4 +1,4 @@
-from dictionary import dict
+from dictionary import dict, allcards
 
 def cardvalue(card): return dict[card[0]] + dict[card[-1]]
 def sortcards(cardlist): return sorted(cardlist, key=cardvalue)
@@ -10,10 +10,14 @@ def ishigher(card1, card2):
 
     return True
 
-def findlegal(hand: list=None, playToBeat: list=None, isStartOfRound: bool=None, playType: int=None, returnSorted: bool=None):
+def countcards(ourDeck: list=None, opponentPlays: list=None):
+    """Attempts to guess remaining cards. Very WIP."""
+    maybeRemain = list(set(opponentPlays).difference(allcards))
+    maybeRemain = list(set(maybeRemain).difference(ourDeck))
+
+def findlegal(hand: list=None, playToBeat: list=None, isStartOfRound: bool=None, playType: int=None):
     """Finds legal moves with your hand and the current play to beat."""
-    hand = sortcards(hand)
-    
+
     if playType == 1: legal = [immigrant for immigrant in hand if ishigher(immigrant, playToBeat[0])]
     if playType == 2: pass # 2-card plays
     if playType == 3: pass # 3-card plays
