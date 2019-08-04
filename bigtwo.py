76ca0fffdda9db6179bfd051130e46749e4f74e1
larrabyte <larrabyte@protonmail.ch>
2019-08-05 08:59:15 +1000

    Start on pairs
    needs more threads

--- bigtwo.py
+++ bigtwo.py
@@ -3,6 +3,25 @@ from dictionary import dict, allcards
 def cardvalue(card): return dict[card[0]] + dict[card[-1]]
 def sortcards(cardlist): return sorted(cardlist, key=cardvalue)
 
+def ispair(card1, card2):
+    if dict[card1[0]] == dict[card2[0]]: return True
+    return False
+
+def ishigherexp(*cards):
+    if all(isinstance(card, str) for card in cards):
+        if dict[cards[0][0]] < dict[cards[1][0]]: return False
+        elif dict[cards[0][0]] == dict[cards[1][0]]:
+            if dict[cards[0][1]] <= dict[cards[1][1]]: return False
+    elif all(isinstance(card, list) for card in cards):
+        if dict[cards[0][0][0]] > dict[cards[1][0][0]]: return True
+        elif dict[cards[0][0][0]] == dict[cards[1][0][0]]:
+            if dict[cards[0][0][1]] == dict[cards[1][0][1]]:
+                pass # At this point, we know that the rank is equal.
+                     # To-do: actually finish
+
+# testing 123
+# print(ishigherexp("8S", "7S"))
+
 def ishigher(card1, card2):
     """Checks whether `card1` is higher than `card2`."""
     if dict[card1[0]] < dict[card2[0]]: return False
