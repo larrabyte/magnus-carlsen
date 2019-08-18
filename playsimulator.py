002705698cf4cb82f356beefecb13556cc3aca01
larrabyte <larrabyte@protonmail.ch>
2019-08-18 23:22:28 +1000

    Worked on game loop
    asyncio? what's that

--- playsimulator.py
+++ playsimulator.py
@@ -45,11 +45,25 @@ class Bigtwo:
         
         while True:
             localplayer = self.players[iter]
+
+            # Quick fix to make self.roundstart false.
+            if self.roundhistory and self.roundstart: self.roundstart = False
+
+            # Return player's play. Check if it actually is in the hand of the bot.
             playedcards = localplayer.play(self.roundstart, self.playtobeat, self.roundhistory, 0, 0, self.roundno)
             if not self.checkcards(localplayer, playedcards): raise RuntimeError("Bot tried to play card not in hand!")
+
+            # If so, delete cards from player's hand and add to history.
             localplayer.delcards([card for card in playedcards])
+            self.addtohistory(localplayer, playedcards)
+
+            # Fancy math to find next player from index.
+            print("Player " + str(localplayer.id) + ": " + str(playedcards) + " " + str(localplayer.hand))
             iter = (iter + 1) % len(self.players)
-            print(playedcards)
+            
+    def addtohistory(self, player, cards):
+        if len(cards) > len(self.playtobeat): self.playtobeat = cards # I mean, the game allows cards to play. its like actual broke tho pls fix
+        self.roundhistory.append((player.id, cards))     
 
     def handoutcards(self):
         cards = random.sample(allcards, k=13)
@@ -57,10 +71,10 @@ class Bigtwo:
         return cards
 
     def startingplayer(self):
-        if self.roundstart:
-            for players in self.players:
-                if "3D" in players.hand:
-                    return players
+        if not self.roundstart: return None
+        for players in self.players:
+            if "3D" in players.hand:
+                return players
 
     def checkcards(self, player, cards):
         for card in cards:
