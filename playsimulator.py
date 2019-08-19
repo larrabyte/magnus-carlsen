600566b748eba42c3ded9ed85899a5f5439705f3
larrabyte <larrabyte@protonmail.ch>
2019-08-19 20:46:33 +1000

    Rewrote playsimulator.py
    Finished game loop and added card checking to revolution()
    
    Only supports one-card plays

--- playsimulator.py
+++ playsimulator.py
@@ -1,85 +1,88 @@
 from importlib import import_module
 from dictionary import allcards
-import itertools as it
+from struct import unpack
+from os import urandom
+from bigtwo import *
 import random
-import struct
-import os
 
 def main():
-    # Prints out initial seed. Useful for repeating games.
-    cryptseed = struct.unpack("I", os.urandom(4))[0]
-    print("Using " + str(cryptseed) + " as seed.")
-    random.seed(cryptseed)
+    # Seed initialisation. Useful for repeating games.
+    crypto = unpack("I", urandom(4))[0]
+    print("Using", crypto, "as seed.")
+    random.seed(crypto)
 
-    game = Bigtwo()
-    game.revolution()
+    bigtwo = Game()
+    for i in range(4): bigtwo.addplayer(i)
+    bigtwo.revolution(bigtwo.startingplayer().id)
 
 class Player:
-    def __init__(self, module, id, hand):
-        if not hasattr(import_module(module), "play"): raise RuntimeError("No play function found.")
-        self.module = import_module(module)
+    def __init__(self, id, hand):
+        if not hasattr(import_module("main"), "play"): raise RuntimeError("No play function found.")
+        self.module = import_module("main")
         self.hand = hand
         self.score = 0
         self.id = id
 
-    def play(self, start, ptb, rh, handsize, scores, roundno):
-        return self.module.play(self.hand, start, ptb, rh, self.id, handsize, scores, roundno)
+    def play(self, roundstart, playtobeat, rndhistory, id, handsize, scores, round):
+        return self.module.play(self.hand, roundstart, playtobeat, rndhistory, id, handsize, scores, round)
 
-    def delcards(self, cards): 
-        for card in cards: self.hand.remove(card)
+    def delcards(self, cards): self.hand = [card for card in self.hand if card not in cards]
+    def addcards(self, cards): self.hand += cards
 
-    def addcards(self, cards): 
-        for card in cards: self.hand.append(card)
-
-class Bigtwo:
+class Game:
     def __init__(self):
-        self.roundno = 0
-        self.players = []
-        self.playtobeat = []
-        self.roundhistory = []
         self.roundstart = True
-        for i in range(4): self.players.append(Player("main", i, self.handoutcards()))
+        self.roundhistory = []
+        self.playtobeat = []
+        self.players = []
+        self.roundno = 0
+        self.passes = 0
 
-    def revolution(self):
-        iter = self.players.index(self.startingplayer())
-        
-        while True:
-            localplayer = self.players[iter]
+    def revolution(self, index):
+        localplayer = self.players[index]
+        if self.roundstart and self.roundhistory: self.roundstart = False
+        if self.passes == 3: raise RuntimeWarning("One revolution has passed.")
 
-            # Quick fix to make self.roundstart false.
-            if self.roundhistory and self.roundstart: self.roundstart = False
+        play = localplayer.play(self.roundstart, self.playtobeat, self.roundhistory, 0, 0, 0, self.roundno)
+        if not self.checkcards(localplayer, play): raise RuntimeError("Bot tried to play card(s) not in hand!")
+        elif self.compareplays(play): self.addhistory(localplayer.id, play)
+        localplayer.delcards(play)
 
-            # Return player's play. Check if it actually is in the hand of the bot.
-            playedcards = localplayer.play(self.roundstart, self.playtobeat, self.roundhistory, 0, 0, self.roundno)
-            if not self.checkcards(localplayer, playedcards): raise RuntimeError("Bot tried to play card not in hand!")
+        print("Player", str(localplayer.id) + ":", play, sortcards(localplayer.hand))
+        self.revolution((index + 1) % len(self.players))
 
-            # If so, delete cards from player's hand and add to history.
-            localplayer.delcards([card for card in playedcards])
-            self.addtohistory(localplayer, playedcards)
+    def compareplays(self, play):
+        if len(play) != len(self.playtobeat):
+            if len(play) == 0: self.passes += 1
+            elif not "3D" in play: raise RuntimeError("Bot exceeded play to beat limit.")
+            else: return True
+            return False
 
-            # Fancy math to find next player from index.
-            print("Player " + str(localplayer.id) + ": " + str(playedcards) + " " + str(localplayer.hand))
-            iter = (iter + 1) % len(self.players)
-            
-    def addtohistory(self, player, cards):
-        if len(cards) > len(self.playtobeat): self.playtobeat = cards # I mean, the game allows cards to play. its like actual broke tho pls fix
-        self.roundhistory.append((player.id, cards))     
+        if len(play) == 1: return ishigher(play[0], self.playtobeat[0])
+        else: return False
 
-    def handoutcards(self):
+    def fetchcards(self):
         cards = random.sample(allcards, k=13)
         for card in cards: allcards.remove(card)
         return cards
 
-    def startingplayer(self):
-        if not self.roundstart: return None
-        for players in self.players:
-            if "3D" in players.hand:
-                return players
+    def addhistory(self, id, cards):
+        self.roundhistory.append((id, cards))
+        self.playtobeat = cards
+
+    def addplayer(self, id): 
+        self.players.append(Player(id, self.fetchcards())) 
 
     def checkcards(self, player, cards):
         for card in cards:
-            if not card in player.hand: return False
+            if card not in player.hand: return False
+
         return True
 
-if __name__ == "__main__":
-    main()
+    def startingplayer(self):
+        for player in self.players:
+            if "3D" in player.hand: return player
+
+        return None
+
+if __name__ == "__main__": main()
