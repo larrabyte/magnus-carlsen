2821a361f2329e7f7c474ab3a91305ea2bdc2abf
larrabyte <larrabyte@protonmail.ch>
2019-08-20 18:12:14 +1000

    Added examples to timings.py
    ayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- timings.py
+++ timings.py
@@ -1,6 +1,15 @@
+from dictionary import allcards
+from bigtwo import *
 import timeit
+import random
 
-def timefunc(function, *parameters):
+def speedtest(function, *parameters):
+    """Prints the time taken to perform `function` with `parameters`.\n
+       Useful for determining the speed of certain functions."""
+    
     firstrec = timeit.default_timer()
     function(*parameters)
     print(function.__name__ + ": " + str((timeit.default_timer() - firstrec) * 1000) + " milliseconds.")
+
+# Example of how to use (should return 0.01ms):
+# speedtest(ishigher, allcards[0], allcards[1])
